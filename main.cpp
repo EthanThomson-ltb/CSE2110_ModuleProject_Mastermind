78643476c9845895a1fd3989dfcb7ad62085c11d
EthanThomson-ltb <ltbluet@gmail.com>
2026-02-13 10:00:52 -0700

    Almost functional, missing green-yellow-red function.

--- main.cpp
+++ main.cpp
@@ -11,6 +11,16 @@ bool winCondition(int in1,int in2,int in3, int ran1, int ran2, int ran3) {
     return false;
 }
 
+bool inputVal(int inMain, int in1, int in2, int in3) {
+    if (inMain < 111 || inMain > 999) {
+        return false;
+    }
+    if (in1 == 0 || in2 == 0 || in3 == 0) {
+        return false;
+    }
+    return true;
+}
+
 int main() {
     cout << "Welcome to Mastermind!" << endl << "I am thinking of a 3 digit number, no repeated digits." << endl <<
             "Can you guess what I'm thinking? You have 10 guesses..." << endl;
@@ -27,17 +37,21 @@ int main() {
     int i = 0;
     while (winCondition(in1, in2, in3, ran1, ran2, ran3) == false) {
 
-        cout << "Enter your 3 digit guess using 1-9 only: ";
+        cout << endl << "Enter your 3 digit guess using 1-9 only: ";
         cin >> inMain;
         i++;
         in3 = inMain % 10;
         in2 = (inMain / 10) % 10;
         in1 = (inMain / 100) % 10;
-        cout << "Guess " << i << ": " << inMain << "    ";
+        if (inputVal(inMain, in1, in2, in3)) {
+            cout << "Guess " << i << ": " << inMain << "    ";
 
 
+        } else {
+            cout << "Please enter as a valid number" << endl;
+        }
     }
-    cout << endl << "Congratulation! You took " << i << " guesses.";
+    cout << endl << endl << "Congratulations! You took " << i << " guesses.";
 
     return 0;
 }
